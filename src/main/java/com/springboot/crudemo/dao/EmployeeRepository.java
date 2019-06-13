d9a7b2958fef3e30aaa68f8053903cf8ff9956a0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-13 22:29:48 +0100

    Configuration, Paginating and Sorting
    Demo

--- src/main/java/com/springboot/crudemo/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/crudemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.crudemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.crudemo.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
